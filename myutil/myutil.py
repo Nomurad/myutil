c1ecd6f5c886c1232fc48a6d8cc2ba410f2b9fd4
seiya nomura <nomurad@outlook.jp>
2020-01-13 01:30:59 +0900

    add stopwatch

--- myutil/myutil.py
+++ myutil/myutil.py
@@ -1,4 +1,32 @@
 import os 
+import time
+from datetime import datetime
+from contextlib import contextmanager
+
+def parse_time(a):
+    if 0 < a < 1:
+        return f'{a:.3g}秒'
+    sec = int(a)
+    d = f'{sec // 86400}日'          if sec >= 86400 else ''
+    h = f'{sec % 86400 // 3600}時間' if sec >= 3600  else ''
+    m = f'{sec % 3600 // 60}分'      if sec >= 60    else ''
+    s = f'{sec % 60}秒'
+    return d + h + m + s
+
+@contextmanager
+def stopwatch(name='anonymous'):
+    start = time.time()
+    yield lambda: time.time() - start
+    elapsed_time = time.time() - start
+    isec = int(elapsed_time)
+    if isec < 60:
+        stime = f'{elapsed_time:.3g}秒'
+    else:
+        stime = f'{isec}秒 ({parse_time(isec)})'
+    print(f'[Stopwatch@{strnow()}] {name}: {stime}')
+
+def strnow(format='%Y/%m/%d %H:%M:%S'):
+    return datetime.now().strftime(format)
 
 class Term_Colors(object):
     BLACK     = "\033[30m"
